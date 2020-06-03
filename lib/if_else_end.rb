7e051ae6d63cad0a95f6365ff5a65b43b4461286
Eli Landau <eli.b.landau@gmail.com>
2020-06-03 14:00:02 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,9 @@
 # Write your solution here
+chance_if_rain = 0.2
+puts "let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella"
+else
+  puts "enjoy the fine day"
+end
+puts "Oh i forgot, always wear sunblock"
