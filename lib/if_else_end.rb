5132e573b7e831d00945531cd57f21bee87a269e
Eli Landau <eli.b.landau@gmail.com>
2020-06-03 14:10:02 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,1 @@
 # Write your solution here
-chance_of_rain = -40
-puts "let's go outside!"
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack and umbrella"
-else
-  puts "stay home and read Poe."
-end
